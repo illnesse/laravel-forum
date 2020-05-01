b06044dca06f95a5b76427ce5f9e6bea72bbf42d
sbrendel <illnesse@gmail.com>
2020-05-01 11:18:05 +0200

    news index route

--- routes/news.php
+++ routes/news.php
@@ -1,6 +1,6 @@
 <?php
 $ns = config('forum.news.router.namespace');
-$r->get('/', ['as' => 'category.show', 'uses' => $ns.'CategoryController@shownews']);
+$r->get('/', ['as' => 'index', 'uses' => $ns.'CategoryController@shownews']);
 $r->get('new', ['as' => 'new.index', 'uses' => $ns.'ThreadController@indexNew']);
 $r->patch('new', ['as' => 'new.mark-read', 'uses' => $ns.'ThreadController@markNew']);
 
